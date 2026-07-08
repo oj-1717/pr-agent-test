61463befa5bb6077d33b71a7482439b673c987e3
Jaswal <ojaswi.jaswal@accenture.com>
2026-07-09 02:28:13 +0530

    2

--- db.py
+++ db.py
@@ -3,3 +3,15 @@ def connect_to_database():
     password = "supersecret123"
     print(f"Connecting as {username}...")
     return {"user": username, "pass": password}
+
+
+
+
+
+
+print("idiot")
+    print("idiot")
+
+
+password = "admin123"
+secret_key = "my-secret-key"
